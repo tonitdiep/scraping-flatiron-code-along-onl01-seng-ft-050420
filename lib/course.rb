b99195653d14231f4518cd44ce10b28e6f4b3a32
tonitdiep <adiep123@gmail.com>
2020-05-22 15:32:24 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,14 @@
-
+class Course
+    @@all = []
+    attr_accessor :title, :schedule, :description 
+    def initialize
+        @title = title 
+        @schedule = schedule
+        @description = description 
+        @all << self
+    end
+    def self.all
+        @@all
+    end
+end
 
